42bd221338d92512cb98b3b05c8fe8b2c8d2881f
徐亮亮 <1156033582@qq.com>
2022-08-24 21:44:33 +0800

    web init

---
+++ src/web/config/routes.ts
@@ -0,0 +1,69 @@
+﻿export default [
+  {
+    path: '/user',
+    layout: false,
+    routes: [
+      {
+        name: 'login',
+        path: '/user/login',
+        component: './User/Login',
+      },
+      {
+        component: './404',
+      },
+    ],
+  },
+  {
+    path: '/welcome',
+    name: 'welcome',
+    icon: 'smile',
+    component: './Welcome',
+  },
+  {
+    path: '/admin',
+    name: 'admin',
+    icon: 'crown',
+    access: 'canAdmin',
+    routes: [
+      {
+        path: '/admin/sub-page',
+        name: 'sub-page',
+        icon: 'smile',
+        component: './Welcome',
+      },
+      {
+        component: './404',
+      },
+    ],
+  },
+  {
+    path: '/test',
+    name: 'test',
+    icon: 'crown',
+    access: 'canAdmin',
+    routes: [
+      {
+        path: '/test/NewPage',
+        name: 'sub-page',
+        icon: 'smile',
+        component: './test/NewPage',
+      },
+      {
+        component: './404',
+      },
+    ],
+  },
+  {
+    name: 'list.table-list',
+    icon: 'table',
+    path: '/list',
+    component: './TableList',
+  },
+  {
+    path: '/',
+    redirect: '/welcome',
+  },
+  {
+    component: './404',
+  },
+];
